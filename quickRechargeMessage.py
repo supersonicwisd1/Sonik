f6278e629c926656c187b7937f8a7079b10f7c6b
supersonicwisd1 <60221897+supersonicwisd1@users.noreply.github.com>
2021-08-18 06:57:12 +0200

    Update quickRechargeMessage.py

--- quickRechargeMessage.py
+++ quickRechargeMessage.py
@@ -7,7 +7,7 @@ def quickMessage(amount):
     time.sleep(10)
     print('######################################################################\n')
     input('\nMessage!!! from Supersonik, press enter to open')
-    print('\nSUpersonik bank')
+    print('\nSupersonik bank')
     print('Debit Message\n')
     print('You just recharge your mobile phone with the sum of', 'N' + str(amount), '.\nYou have been debited by',
           str(amount), 'on', time.asctime())
